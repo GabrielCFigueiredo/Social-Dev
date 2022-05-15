11c1451151a119d180c3c1c19770f94d71a36407
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-05-15 11:50:14 -0300

    Instalado e Configurado Styled-Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { GlobalStyles } from "../globalStyles/globalStyles"
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <GlobalStyles/>
+    <Component {...pageProps} />
+    </>
+  )
+   
 }
 
 export default MyApp
