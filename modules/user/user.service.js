ba10c42f9e64e53a89dfad69c29b2f4a647c20f5
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-05-22 10:17:22 -0300

    Criado middleware para conexão com o bd

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,6 +1,5 @@
-
 export const SignupUser = (body) => {
-    console.log("Signup User");
-    console.log(body);
-    return true
-}
+  console.log("Signup User");
+  console.log(body);
+  return true;
+};
--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,21 +1,21 @@
-import Joi from "joi"
-import connect from "next-connect"
-import validate from "../../../lib/midlleWares/validation"
-import { SignupUser } from "../../../modules/user/user.service"
-
+import Joi from "joi";
+import createHandler from "../../../lib/middleWares/nextConnect";
+import validate from "../../../lib/middleWares/validation";
+import { SignupUser } from "../../../modules/user/user.service";
 
 const postSchema = Joi.object({
-    firstName: Joi.string().required().max(50),
-    lastName: Joi.string().required().max(50),
-    user: Joi.string().required().max(30),
-    email: Joi.string().email().required().max(100),
-    password: Joi.string().required().max(50).min(6)
-})
-const Signup = connect()
-Signup.post(validate({ body: postSchema }),(req, res) => {
+  firstName: Joi.string().required().max(50),
+  lastName: Joi.string().required().max(50),
+  user: Joi.string().required().max(30),
+  email: Joi.string().email().required().max(100),
+  password: Joi.string().required().max(50).min(6),
+});
+const signup = createHandler().post(
+  validate({ body: postSchema }),
+  (req, res) => {
+    SignupUser(req.body);
+    res.status(200).json({ teste: "ok" });
+  }
+);
 
-    SignupUser(req.body)
-    res.status(200).json({teste: "ok"})
-})
-
-export default Signup
+export default signup;
